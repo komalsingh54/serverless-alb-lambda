a91ce66d86c3901034ab9f6a59818165023640ab
Komal Singh <komalsingh54@gmail.com>
2021-03-14 19:58:15 +0530

    AWS ALB-lambda push

---
+++ testHandler.js
@@ -0,0 +1,10 @@
+
+module.exports.handler = async (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    statusDescription: '200 OK',
+    headers: { 'Set-cookie': 'cookies', 'Content-Type': 'application/json' },
+    body: 'Successfully executed "test" lambda call via loadbalancer',
+  };
+  return callback(null, response);
+}
